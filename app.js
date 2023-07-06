b5279d1d9205eb071f04f9de0e55136206d5f2de
Paulina-Ensastiga <celypau@gmail.com>
2023-07-06 13:32:05 -0600

    Ejercicio live-coding

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+//Celia Paulina Ensastiga Pichardo
 // Escribe tu código aquí.
+
+function BigWords (string, array){
+    const Stpalabra = [];
+    for (let  i= 0; i<string; length++){
+    }
+    
+        return result;
+    } 
+    
+    const myArray= [ 'insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+        console.log(myArray);
+    
+    /*Revisa*/
+    function ArrayPalabras(string, array){
+    const ul = document.getElementsByName("ul");
+    for (let  i= 0; i<string; length++){
+        /*Elemento li*/
+    const li = document.createElement ("li")
+    
+    li.appendChild("li");
+    } 
+    }
+    
+    /*
+    Se ve que Pau puede realizar las funciones, sin embargo aún hay un poco de confusión con lo de manipulación del DOM. Pero pudo hacer que en consola le imprimera el array. Calificación 4
+    */
